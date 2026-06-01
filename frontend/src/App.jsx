e5de0f7d1b42f4313a88255b50b6c9b59e485547
Prince Titiya <princetitiya145@gmail.com>
2026-06-02 02:50:14 +0530

    ERC721 page integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import ERC1155 from './pages/ERC1155.jsx';
 import Inventory from './pages/Inventory.jsx';
 import MintLab from './pages/MintLab.jsx';
 import ERC20 from './pages/ERC20.jsx';
+import ERC721 from './pages/ERC721.jsx';
 import Navbar from './components/Navbar.jsx';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/inventory" element={<Inventory />} />
           <Route path="/mint-lab" element={<MintLab />} />
           <Route path="/erc20" element={<ERC20 />} />
+          <Route path="/erc721" element={<ERC721 />} />
         </Routes>
       </main>
     </>
