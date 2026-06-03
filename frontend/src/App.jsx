4747ce3a1792f90e4c4bc30ba9e6fbc51551bb5b
Prince Titiya <princetitiya145@gmail.com>
2026-06-03 20:35:02 +0530

    frontend changes and dependencies added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import ERC1155 from './pages/ERC1155.jsx';
 import Inventory from './pages/Inventory.jsx';
 import MintLab from './pages/MintLab.jsx';
 import ERC20 from './pages/ERC20.jsx';
 import ERC721 from './pages/ERC721.jsx';
+import Compare from './pages/Compare.jsx';
 import Navbar from './components/Navbar.jsx';
 
 function App() {
@@ -12,11 +13,13 @@ function App() {
       <Navbar />
       <main className="pt-20">
         <Routes>
-          <Route path="/" element={<ERC1155 />} />
+          <Route path="/" element={<Navigate to="/compare" replace />} />
+          <Route path="/erc1155" element={<ERC1155 />} />
           <Route path="/inventory" element={<Inventory />} />
           <Route path="/mint-lab" element={<MintLab />} />
           <Route path="/erc20" element={<ERC20 />} />
           <Route path="/erc721" element={<ERC721 />} />
+          <Route path="/compare" element={<Compare />} />
         </Routes>
       </main>
     </>
