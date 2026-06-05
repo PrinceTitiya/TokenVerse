72d72e1566a0fce4d0beae8f9bbf6c96565899ae
Prince Titiya <princetitiya145@gmail.com>
2026-06-05 21:04:45 +0530

    solidity verison upgraded

--- script/ERC1155/DeployTokenVerse1155.s.sol
+++ script/ERC1155/DeployTokenVerse1155.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.26;
 
 import {Script, console} from "forge-std/Script.sol";
 import {TokenVerse1155} from "../../src/ERC1155/TokenVerse1155.sol";
